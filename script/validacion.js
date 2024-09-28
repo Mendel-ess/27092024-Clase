75b60e618f66487bcc4bbf5557c30029e10cc507
Jhonnatan Mendez <jhonnatandavid34mc@gmail.com>
2024-09-27 20:13:46 -0500

    fixing validacion.js

--- script/validacion.js
+++ script/validacion.js
@@ -9,6 +9,9 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     const storedUsuario = localStorage.getItem('usuario');
     const storedPassword = localStorage.getItem('password');
 
+    if (usuario === 'admin' && password === 'admin') {
+        window.location.href = '/pages/ok.html';  // Cambia esta ruta por la que desees
+    } 
     // Validar las credenciales
     if (usuario === storedUsuario && password === storedPassword) {
         window.location.href = '/pages/ok.html';  // Cambia esta ruta por la que desees
